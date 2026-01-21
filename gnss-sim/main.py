b55a60e1694e0954f88d61580c55346e6da7dc26
ammxr <ammarhakim57@gmail.com>
2026-01-21 16:22:48 -0500

    Update main.py Publishing

--- gnss-sim/main.py
+++ gnss-sim/main.py
@@ -1,7 +1,9 @@
-# OpenSky data (LLA format) →  coordinates conversion (ECEF) → satellites / pseudoranges (calculations) → IQ signals (signal compilation) → ZeroMQ (final publishing)
+#
+OpenSky data (LLA format) →  coordinates conversion (ECEF) → satellites / pseudoranges (calculations) → IQ signals (signal compilation) → ZeroMQ (final publishing)
 from time import time
 from opensky_route_fetcher import get_active_icao, fetch_flight_track
 from lla_to_ecef import lla_to_ecef
+from zeromq_publisher import publish_iq
 
 print("Starting GNSS Signal Simulation")
 print("----- Fetching Live Flight Track -----")
@@ -31,11 +33,21 @@ for point in track_lla:
     lat = point["lat"]
     lon = point["lon"]
     alt = point["alt"]
+
+    metadata = [time, lat, lon, alt]
     print("Attempting conversion", count, "/", total_points, "for LLA:   [" , lat , "        " , lon, "         ",  alt,"]")
     result_ecef = lla_to_ecef(lat, lon, alt)
+    result_publish = [ecef, metadata]
     print("Conversion Successful ECEF Coordinates: ", result_ecef)
     
-    track_ecef.append(result_ecef)
+    track_ecef.append(result_publish)
     print("-----------------------------------------------------------------------------------")
 
-print("\n\nFlight Track Conversion Complete " , count , "/", total_points, "\n\n")
+print("\n\nFlight Track Conversion Complete " , count , "/", total_points, "\n\n")i
+
+print("----- Initializing Publisher -----")
+for ecef_data in track_ecef:
+    # ecef_data[0] contains actual ECEF point, ecef_data[1] contains lla and time metadata for debug tracing
+    publish_iq("GNSS", ecef_data[0], ecef_data[1])
+    print("Publishing to Topic: GNSS",  " with [Frame, Metadata]: ", ecef_data)
+
